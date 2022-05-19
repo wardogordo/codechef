e0f9ae7061b8ddadf4f432f5e119cfdf091b92bd
wardogordo <wardh@me.com>
2022-05-19 15:35:44 -0500

    Working.

--- flow006/main.cpp
+++ flow006/main.cpp
@@ -6,16 +6,18 @@ int main()
     int mine{54321};
     int t{5};
     std::vector<int> number;
-//    number.push_back(8);
+
     number.push_back(mine / 1 % 10);
     number.push_back(mine / 10 % 10);
     number.push_back(mine / 100 % 10);
     number.push_back(mine / 1000 % 10);
     number.push_back(mine / 10000 % 10);
-
+    int tem{0};
     for (auto i : number)
     {
-        std::cout << i;
+        tem += i;
+        std::cout << i << '\n';
+        std::cout << "TOTAL: " << tem << '\n';
     }
 
 }
