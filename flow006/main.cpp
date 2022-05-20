05734ff463c200e12da399669d4335e4f2d6bce5
wardogordo <wardh@me.com>
2022-05-19 23:37:09 -0500

    Still working. 10^0 == 1.

--- flow006/main.cpp
+++ flow006/main.cpp
@@ -3,10 +3,16 @@
 
 int main()
 {
-    int mine{54321};
+    int mine{54021};
     int t{5};
     std::vector<int> number;
 
+
+    std::cout << mine / 100;
+
+/*
+
+    // if mine / some 10^x == 0, exit the loop.
     number.push_back(mine / 1 % 10);
     number.push_back(mine / 10 % 10);
     number.push_back(mine / 100 % 10);
@@ -16,8 +22,9 @@ int main()
     for (auto i : number)
     {
         tem += i;
-        std::cout << i << '\n';
+        std::cout << "Digit: " << i << '\n';
         std::cout << "TOTAL: " << tem << '\n';
     }
+*/
 
 }
