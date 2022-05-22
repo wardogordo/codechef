89ba93aeaf69b720f768e8d542bc230fcb747619
wardogordo <wardh@me.com>
2022-05-22 12:59:23 -0500

    Almost complete. Working on count of integer digits.

--- flow006/main.cpp
+++ flow006/main.cpp
@@ -5,42 +5,21 @@ int main()
 {
     int t;
     std::cin >> t;
-    std::vector<int> number;
 
     while (t--)
     {
         int myInt{};
         std::cin >> myInt;
-        for (int i = 0; i < 5; ++i)
+        int total{};
+        std::vector<int> number;
+        for (int i = 0; i < (log10(myInt) + 1); ++i)
         {
-            int tem1;
-            tem1 = (int)pow(10, i);
-            std::cout << "tem1: " << tem1 << '\n';
-            // if myInt / some 10^x == 0, exit the loop.
-            number.push_back(myInt / tem1 % 10);
-            number.push_back(myInt / 10 % 10);
-            number.push_back(myInt / 100 % 10);
-            number.push_back(myInt / 1000 % 10);
-            number.push_back(myInt / 10000 % 10);
+            int tem;
+            tem = (int)pow(10, i);
+            number.push_back(myInt / tem % 10);
+            total += number[i];
         }
-        int tem{0};
-        for (int i = 4; i >=0; --i)
-        {
-            std::cout << "number: " << number[i] << '\n';
-            tem += number[i];
-        }
-        std::cout << "sum: " << tem << '\n';
-/*
-
-        int tem{0};
-           for (auto i : number)
-           {
-               tem += i;
-               std::cout << "Digit: " << i << '\n';
-               std::cout << "TOTAL: " << tem << '\n';
-           }
-*/
-
+        std::cout << total << '\n';
     }
 
     return 0;
