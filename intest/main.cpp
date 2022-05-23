d4ec5adc9e0c5bc26d91cd257e616be049c3cd59
wardogordo <wardh@me.com>
2022-05-23 07:17:33 -0500

    Completed.

--- intest/main.cpp
+++ intest/main.cpp
@@ -4,5 +4,17 @@ int main()
 {
     int n, k;
     std::cin >> n >> k;
+
+    int count{0};
+    for (int i = 0; i < n; ++i)
+    {
+        int t;
+        std::cin >> t;
+        if (t % k == 0)
+        {
+            count ++;
+        }
+    }
+    std::cout << count << '\n';
     return 0;
 }
